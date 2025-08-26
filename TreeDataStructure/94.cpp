f94f066f33a99e310c8668bb70ad2d58cae8e907
Ayush Gurjar <jiaayush256@gmail.com>
2025-08-26 20:32:45 +0530

    Update 94.cpp

--- TreeDataStructure/94.cpp
+++ TreeDataStructure/94.cpp
@@ -1,3 +1,4 @@
+//moris trvercel
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -27,3 +28,31 @@ public:
     return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> res;                  // to store result
+//         stack<TreeNode*> st;              // stack to simulate recursion
+//         TreeNode* curr = root;
+
+//         while(curr != NULL || !st.empty()) {
+//             // 1. Go as left as possible
+//             while(curr != NULL) {
+//                 st.push(curr);
+//                 curr = curr->left;
+//             }
+
+//             // 2. Process the node
+//             curr = st.top(); 
+//             st.pop();
+//             res.push_back(curr->val);
+
+//             // 3. Move to right subtree
+//             curr = curr->right;
+//         }
+//         return res;
+//     }
+// };
