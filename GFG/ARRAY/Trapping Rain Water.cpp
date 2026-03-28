9512caea351f706ab1f17fdf449d2e20c4ded2d6
Ayush Gurjar <jiaayush256@gmail.com>
2026-03-29 01:35:53 +0530

    Trapping Rain Water.cpp

--- GFG/ARRAY/Trapping Rain Water.cpp
+++ GFG/ARRAY/Trapping Rain Water.cpp
@@ -35,3 +35,54 @@ class Solution {
         return water;
     }
 };
+
+
+
+
+
+----------------------------------------------------------
+
+class Solution {
+  public:
+  
+    vector<int> getLeftMaxArray(vector<int>& arr, int& n){
+        vector<int> leftMax(n);
+        
+        leftMax[0] = arr[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = max(leftMax[i-1], arr[i]);
+        }
+        
+        return leftMax;
+    }
+    
+    vector<int> getrightMaxArray(vector<int>& arr, int& n){
+        vector<int> rightMax(n);
+        
+        rightMax[n-1] = arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = max(rightMax[i+1], arr[i]);
+        }
+        
+        return rightMax;
+    }
+    
+  
+    int maxWater(vector<int> &arr) {
+        
+        int n = arr.size();
+        
+        vector<int> leftMax  = getLeftMaxArray(arr, n);
+        vector<int> rightMax = getrightMaxArray(arr, n);
+        
+        int sum = 0;
+        
+        for(int i=0; i<n; i++){
+            int h = min(leftMax[i], rightMax[i]) - arr[i];
+            
+            sum += h;
+        }
+        
+        return sum;
+    }
+};
